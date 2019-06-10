89eaa5c72ca9363abe2ec6ade2ee237268dc739e
decaf <rarunok@yahoo.com>
2019-06-11 02:31:51 +0800

    :recycle: flatten files

---
+++ LongestPalindrome.py
@@ -0,0 +1,23 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        if len(s) < 2:
+            return s
+        lo = 0
+        max_len = 0
+        for i in range(len(s)):
+            odd_len = self.palindrome_len_at(s, i, i)
+            even_len = self.palindrome_len_at(s, i, i+1)
+            if max(odd_len, even_len) > max_len:
+                if odd_len >= even_len:
+                    lo = i - odd_len//2
+                    max_len = odd_len
+                else:
+                    lo = i - even_len//2 + 1
+                    max_len = even_len
+        return s[lo:lo + max_len]
+
+    def palindrome_len_at(self, s: str, j: int, k: int) -> int:
+        while (j >= 0 and k < len(s) and s[j] == s[k]):
+            j -= 1
+            k += 1
+        return k - j - 1
