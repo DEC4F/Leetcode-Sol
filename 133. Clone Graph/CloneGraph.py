fce60cd551455cd7743ba4c9d65b3fd8afef0818
decaf <rarunok@yahoo.com>
2019-09-12 20:59:35 -0500

    solved p101

---
+++ 133. Clone Graph/CloneGraph.py
@@ -0,0 +1,14 @@
+"""
+Given a reference of a node in a connected undirected graph, return a deep copy (clone) of the graph. Each node in the graph contains a val (int) and a list (List[Node]) of its neighbors.
+"""
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val, neighbors):
+        self.val = val
+        self.neighbors = neighbors
+"""
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        pass
