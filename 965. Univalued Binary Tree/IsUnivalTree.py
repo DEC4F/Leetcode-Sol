815efd0b91a77d97b8ddc46f311b8c711825dab2
decaf <rarunok@yahoo.com>
2019-10-28 16:33:50 -0500

    :sparkles: solved P111 P965

---
+++ 965. Univalued Binary Tree/IsUnivalTree.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def isUnivalTree(self, root: TreeNode) -> bool:
+        """
+        T(n) = O(n) -- visit all nodes in worst case
+        S(n) = O(n) -- size of recursion stack in totally unbalanced tree
+        """
+        def dfs(node: TreeNode) -> bool:
+            nonlocal root
+            if node is None:
+                return True
+            if node.val != root.val:
+                return False
+            return dfs(node.left) and dfs(node.right)
+        return dfs(root)
