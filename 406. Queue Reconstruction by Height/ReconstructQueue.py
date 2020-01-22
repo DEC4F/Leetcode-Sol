6909cca2fa06b95d1292fddd71189a7a50eb61ff
DEC4F <rarunok@yahoo.com>
2020-01-22 03:07:27 -0600

    solved another greedy prob

---
+++ 406. Queue Reconstruction by Height/ReconstructQueue.py
@@ -0,0 +1,16 @@
+
+"""
+Suppose you have a random list of people standing in a queue. Each person is described by a pair of integers (h, k), where h is the height of the person and k is the number of people in front of this person who have a height greater than or equal to h. Write an algorithm to reconstruct the queue.
+"""
+
+class Solution:
+    def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+        """
+        T(n) = O(n^2) = nlogn + n^2 -- presort plus insert every person
+        S(n) = O(n) -- size of res
+        """
+        people.sort(key = lambda x: (-x[0], x[1]))
+        res = []
+        for p in people:
+            res.insert(p[1], p)
+        return res
